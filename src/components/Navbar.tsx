329123969afcf2b0cfa2df0ec5a536b570020291
symien moore <symien.moore@gmail.com>
2022-09-14 20:11:35 -0400

    🚀

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,49 @@
 import React from 'react'
+import { useAuth } from '../firebase/firebaseContext'
 
 const Navbar = () => {
-  return (
-    <div>
-        <div className="navbar bg-base-100">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
+    const {currentUser, signIn} = useAuth()
+    if(currentUser) {
+        return (
+          <div>
+              <div className="navbar bg-base-100">
+                  <div className="flex-none">
+                      <button className="btn btn-square btn-ghost">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                      </button>
+                  </div>
+                  <div className="flex-1">
+                      <a className="btn btn-ghost normal-case text-xl">Surf</a>
+                  </div>
+                  <div className="flex-none">
+                      <button className="btn btn-square btn-ghost">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                      </button>
+                  </div>
+              </div>
+          </div>
+        )
+    } else {
+        return (
+            <div>
+                <div className="navbar bg-base-100">
+                    <div className="flex-none">
+                        <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </button>
+                    </div>
+                    <div className="flex-1">
+                        <a className="btn btn-ghost normal-case text-xl">Surf</a>
+                    </div>
+                    <div className="flex-none">
+                        <button className="btn" onClick={signIn}>
+                            Login
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">Surf</a>
-            </div>
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                </button>
-            </div>
-        </div>
-    </div>
-  )
+          )
+    }
 }
 
 export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useAuth } from "../firebase/firebaseContext";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      
-    </>
-  );
+  const {currentUser} = useAuth()
+  if(currentUser) {
+    return (
+      <>
+        <h2>Welcome {currentUser.displayName}</h2>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1 className="bold">Welcome to Surf!</h1>
+        Sign in and share your stories.
+      </>
+    )
+  }
 };
 
 export default Home;
